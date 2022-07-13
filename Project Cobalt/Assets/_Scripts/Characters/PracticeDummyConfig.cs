d1ada8bc4fa188323578f89bab44f126dae7f62b
Pihl17 <mpihl17@student.aau.dk>
2022-07-13 13:29:36 +0200

    Tech Demo stage and practise dummies

---
+++ Project Cobalt/Assets/_Scripts/Characters/PracticeDummyConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Practice Dummy Config", menuName = "ScriptableObject/Practice Dummy Config", order = 0)]
+public class PracticeDummyConfig : ScriptableObject
+{
+
+    [SerializeField] float maxHealth = 1;
+    public float MaxHealth { get { return maxHealth; } }
+    [SerializeField] float respawnTime = 1;
+    public float RespawnTime { get { return respawnTime; } }
+
+}
---
+++ Project Cobalt/Assets/_Scripts/Characters/PracticeDummyScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeDummyScript : MonoBehaviour, IDestructible
+{
+
+    public PracticeDummyConfig config;
+    
+    public event HealthChangeEvent OnHealthChanged;
+	public event DestroyedEvent OnDestroy;
+
+    float health;
+
+    MeshRenderer render;
+    Collider col;
+    Vector3 spawnPoint;
+
+	public void Damage(float amount) {
+        health = Mathf.Clamp(health - amount, 0, 10);
+        OnHealthChanged?.Invoke(health, amount);
+        if (health <= 0)
+            OnDestroy?.Invoke();
+    }
+
+    void Die() {
+        StartCoroutine(Respawn());
+    }
+
+	// Start is called before the first frame update
+	void Start() {
+        health = config.MaxHealth;
+        OnDestroy += Die;
+        render = GetComponent<MeshRenderer>();
+        col = GetComponent<Collider>();
+        spawnPoint = transform.position;
+    }
+
+    IEnumerator Respawn() {
+        render.enabled = false;
+        col.enabled = false;
+        transform.position = new Vector3(-9999, -9999, -9999);
+        yield return new WaitForSeconds(config.RespawnTime);
+        render.enabled = true;
+        col.enabled = true;
+        health = config.MaxHealth;
+        transform.position = spawnPoint;
+    }
+
+}
